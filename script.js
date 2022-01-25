64288301761d6579296338e33ba75b793955617b
regiscfreitas <regiscfreitas1212@gmail.com>
2022-01-25 20:31:15 -0300

    adding a delete button and making the checkbox work to say that a task is done, not responsive yet

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addTaskButton = document.getElementById("addTaskButton");
 const addTaskInput = document.getElementById("newTask");
 const taskList = document.querySelector("ul");
+const deleteTaskButton = document.getElementsByClassName("deleteButton");
 
 
 function inputLenght(){
@@ -8,9 +9,35 @@ function inputLenght(){
 }
 function createListElement(){
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(addTaskInput.value));
+    let listItemLabel =  document.createElement("span");
+    listItemLabel.innerText = addTaskInput.value;
+    li.appendChild((listItemLabel));
+    let doneCheckbox = document.createElement("input");
+    doneCheckbox.type = "checkbox";
+    doneCheckbox.classList.add("undone");
+    doneCheckbox.onclick = function(){
+        if(!(doneCheckbox.parentElement.className == "done")){
+            doneCheckbox.parentElement.classList.remove("undone");
+            doneCheckbox.parentElement.classList.add("done");   
+        }else{
+            doneCheckbox.parentElement.classList.remove("done");
+            doneCheckbox.parentElement.classList.add("undone");
+        }
+    };
+    li.appendChild(doneCheckbox);
+    let deleteTask = document.createElement("button");
+    deleteTask.setAttribute("class","deleteButton")
+    let deleteTaskIcon = document.createElement("img")
+    deleteTask.appendChild(deleteTaskIcon);
+    deleteTaskIcon.src = "trash.png";
+    li.appendChild(deleteTask);
     taskList.appendChild(li);
     addTaskInput.value = "";
+}
+function responsiveListElement(){
+    if(inputLenght() > 40){
+        createListElement();
+    }
 }
 function addListAfterKeyPress(event){
         if(event.which === 13 && inputLenght() > 0){
@@ -26,6 +53,16 @@ function addListAfterClick() {
 addTaskInput.addEventListener("keypress", addListAfterKeyPress);
 addTaskButton.addEventListener("click", addListAfterClick);
 
+/*function taskDone(){
+    document.querySelector("li").classList.add("done");
+}
+
+doneCheckbox.addEventListener("click", taskDone);*/
 
 
+function deleteTaskFromList(){
+    taskList.removeChild(taskList.lastElementChild); 
+}
+
+document.getElementsByClassName("deleteButton").addEventListener("click", deleteTaskFromList);
 
